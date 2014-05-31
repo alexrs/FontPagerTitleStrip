b33cad805b971b9166def17a5318cccf01ab4b1e
Alejandro Rodríguez Salamanca <alexrs95@gmail.com>
2014-05-31 22:48:45 +0200

    Added comments

--- fontPagerTitleStrip/src/main/java/me/alexrs/fontpagertitlestrip/lib/Typefaces.java
+++ fontPagerTitleStrip/src/main/java/me/alexrs/fontpagertitlestrip/lib/Typefaces.java
@@ -15,51 +15,32 @@ public class Typefaces {
 
     private static final Hashtable<String, Typeface> CACHE = new Hashtable<String, Typeface>();
 
-    /**
-     * @param fontFamily
-     * @return
-     */
+
     public static Typeface get(String fontFamily) {
         Typeface t = get(fontFamily, Typeface.NORMAL);
         return cachedTypeface(fontFamily, t);
     }
 
-    /**
-     * @param familyName
-     * @param style
-     * @return
-     */
+
     public static Typeface get(String familyName, int style) {
         Typeface t = Typeface.create(familyName, style);
         return cachedTypeface(familyName, t);
     }
 
 
-    /**
-     * @param typeface
-     * @param id
-     * @return
-     */
+
     public static Typeface get(Typeface typeface, String id) {
         return cachedTypeface(id, typeface);
     }
 
-    /**
-     * @param context
-     * @param assetPath
-     * @return
-     */
+
     public static Typeface get(Context context, String assetPath) {
         Typeface t = Typeface.createFromAsset(context.getAssets(), assetPath);
         return cachedTypeface(assetPath, t);
 
     }
 
-    /**
-     * @param assetPath
-     * @param t
-     * @return
-     */
+    
     private static Typeface cachedTypeface(String assetPath, Typeface t) {
         synchronized (CACHE) {
             if (!CACHE.containsKey(assetPath)) {
